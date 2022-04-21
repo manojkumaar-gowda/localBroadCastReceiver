6a83ccf7c4c3901ec5c8261703eb06c3d0acff17
= <=>
2022-04-21 14:26:07 +0530

    Init commit

---
+++ app/src/main/java/com/example/localbroadcastreceiver/AirplaneMode.java
@@ -0,0 +1,22 @@
+package com.example.localbroadcastreceiver;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.provider.Settings;
+import android.widget.Toast;
+
+public class AirplaneMode extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        if (isAirplaneModeOn(context.getApplicationContext())) {
+            Toast.makeText(context, "AirPlane mode is on", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(context, "AirPlane mode is off", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private boolean isAirplaneModeOn(Context applicationContext) {
+        return Settings.System.getInt(applicationContext.getContentResolver(), Settings.Global.AIRPLANE_MODE_ON, 0) != 0;
+    }
+}
